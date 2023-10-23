76bd5d808e058f1a1341072cba0a08de75dfb8b4
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-23 18:38:40 -0500

    Added profile_title for seed data

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable
+
+  PROFILE_TITLE = [
+    'Senior Ruby on Rails Developer',
+    'Full Stack Ruby on Rails Developer',
+    'Python Developer',
+    'Node Developer',
+    'Java Developer',
+    'Angular Developer',
+  ].freeze
+end
