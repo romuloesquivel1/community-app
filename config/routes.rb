9fb91146367ec9570b687f160b7dfc99e764d5e5
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-25 11:51:39 -0500

    Added edit members to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get 'member/:id', to: 'members#show', as: 'member'
+  get 'edit_description/:id', to: 'members#edit_description', as: 'edit_member_description'
 end
