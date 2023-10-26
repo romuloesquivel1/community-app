f76d77cf70eb97618a84c5b9d63dd18dbd4fd1b2
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-25 19:18:15 -0500

    Added stimulus username

--- app/javascript/controllers/user_name_controller.js
+++ app/javascript/controllers/user_name_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="user-name"
 export default class extends Controller {
   static targets = ['name']
+  
   connect() {
     console.log('hello world')
   }
