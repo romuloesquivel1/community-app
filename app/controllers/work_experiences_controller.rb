b5976d978bcceb96f321f56ee07cf7eb949c99bb
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-27 17:11:44 -0500

    Fixed bug in work experience controller new method

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -3,7 +3,7 @@ class WorkExperiencesController < ApplicationController
     before_action :set_work_experience, only: %i[edit update destroy]
 
     def new
-        @work_experience = current_user.work_experience.new
+        @work_experience = current_user.work_experiences.new
     end
 
     def edit; end
@@ -21,6 +21,6 @@ class WorkExperiencesController < ApplicationController
     end
 
     def work_experience_params
-        params.require(:work_experience).permit(:start_date, :end_date, :currently_working, :compnay, :employment_type, :location, :location_type, :description, :user_id)
+        params.require(:work_experience).permit(:start_date, :end_date, :currently_working, :compnay, :employment_type, :location, :location_type, :description, :user_id, :job_title)
     end
 end
