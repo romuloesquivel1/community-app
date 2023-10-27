ceae00d32f840c5a891d991a2d89d6b125805855
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-27 14:28:50 -0500

    Added validations to work_experience

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,3 +1,7 @@
 class WorkExperience < ApplicationRecord
   belongs_to :user
+
+  validates :company, :start_date, :employment_type, :job_title, :location, :location_type, presence: true
+  
+  
 end
