13fe38c613194b408a19cc03390876283872382c
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-30 10:16:20 -0500

    Added create method in work experience controller

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -8,7 +8,16 @@ class WorkExperiencesController < ApplicationController
 
     def edit; end
 
-    def create; end
+    def create 
+        @work_experience = current_user.work_experiences.new(work_experience_params)
+        respond_to do |format|
+            if @work_experience.save
+                format.turbo_stream { render turbo_stream: turbo_stream.append('work_exeperience_items', partial: 'work_experiences/work_experience', locals: { work_experience: @work_experience})}
+            else
+                format.turbo_stream { render turbo_stream: turbo_stream.replace('remote_modal', partial: 'shared/turbo_modal', locals: { form_partial: 'work_experiences/form', modal_title: 'Add new experiences'})}
+            end
+        end
+    end
 
     def update; end
 
@@ -21,6 +30,6 @@ class WorkExperiencesController < ApplicationController
     end
 
     def work_experience_params
-        params.require(:work_experience).permit(:start_date, :end_date, :currently_working, :compnay, :employment_type, :location, :location_type, :description, :user_id, :job_title)
+        params.require(:work_experience).permit(:start_date, :end_date, :currently_working, :company, :employment_type, :location, :location_type, :description, :user_id, :job_title)
     end
 end
