be3520ffb35f7f2025766ea2a58e266daa28a7c6
Giovanni Juliao <gjuliao32@gmail.com>
2023-11-06 17:01:17 -0500

    Refactor rspec testing

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.feature "Logins", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+
+  shared_examples 'a successful login' do |login_field|
+    it "should login user with #{login_field}" do
+      user = User.create(
+        email: 'user@example.com',
+        password: 'password',
+        username: 'user123',
+        first_name: 'Alex',
+        last_name: 'User',
+        profile_title: 'Fullstack',
+        confirmed_at: DateTime.now
+      )
+
+      visit root_path
+
+      fill_in 'user_login', with: user.send(login_field)
+      fill_in 'user_password', with: user.password
+
+      click_button 'Log in'
+      expect(page).to have_link('Dev Community')
+      expect(page).to have_link('My Profile')
+      expect(page).to have_link('My Network')
+      expect(page).to have_link('Sign Out')
+
+      expect(page).to have_text('Search proffesionals across the world')
+    end
+  end
+
+  describe 'login' do
+    include_examples 'a successful login', :username
+    include_examples 'a successful login', :email
+  end
 end
